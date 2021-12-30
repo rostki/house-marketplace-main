9efa31780a3ab993d7a5b09ac8c7ae653cdfc053
Coastal Tile & Construction Inc <rost_ki@yahoo.com>
2021-12-30 08:19:45 -0800

    fix4

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,10 +14,10 @@ const firebaseConfig = {
   storageBucket: "house-app-3b325.appspot.com",
   messagingSenderId: "728762286812",
   appId: "1:728762286812:web:ac96f02fd16cb8fdbb2a19",
-  measurementId: "G-GE6X4SS5X3"
+  // measurementId: "G-GE6X4SS5X3"
 };
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const db = getFirestore();
