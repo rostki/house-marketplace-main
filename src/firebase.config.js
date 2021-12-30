bb39841d08c05b3391e9861629827c373dc2d4ab
Coastal Tile & Construction Inc <rost_ki@yahoo.com>
2021-12-30 08:54:56 -0800

    fix5

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -18,6 +18,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const db = getFirestore();
